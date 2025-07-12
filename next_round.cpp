340b31f8299fb67016372c31f9acc34280fc751e
Sabuhi Abbasov Jalal <sebuhiabbasov1514@gmail.com>
2025-07-12 20:24:44 +0400

    Solution to the Day 1 problem A IDDA ICPC Camp

---
+++ next_round.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,k,x,sum=0;
+    cin>>n>>k;
+    while(n--){
+        cin>>x;
+        arr[]
+        
+    }
+    cout<<sum<<endl;
+
+    return 0;
+}
